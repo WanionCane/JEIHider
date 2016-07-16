9ac09c1ea471e8c2b2c7d2cd470b87f548bca1f7
WanionCane <wanioncane@gmail.com>
2016-07-16 13:01:22 -0300

    some changes, added some configs.

--- src/main/java/wanion/jeihider/JEIHider.java
+++ src/main/java/wanion/jeihider/JEIHider.java
@@ -5,9 +5,13 @@ package wanion.jeihider;
  */
 
 import mezz.jei.api.*;
+import mezz.jei.api.recipe.IRecipeCategory;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.Loader;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +20,21 @@ public class JEIHider implements IModPlugin
 {
     private IItemBlacklist itemBlacklist;
     private IItemRegistry iItemRegistry;
-    private final List<ItemStack> itemStacksToHide = new ArrayList<ItemStack>();
+    private final List<ItemStack> itemStacksToHide = new ArrayList<>();
+    private final boolean hideRecyclerRecipes;
+    private final boolean hideEnchantmentRecipes;
+    private final Class<? extends IRecipeCategory> recyclerCategoryClass;
+    private final Class<? extends IRecipeCategory> enchantmentCategoryClass;
+
+    @SuppressWarnings("unchecked")
+    public JEIHider() throws ClassNotFoundException
+    {
+        final Configuration config = new Configuration(new File("." + File.separatorChar + "JEI Hider.cfg"));
+        hideRecyclerRecipes = Loader.isModLoaded("IC2") && config.get("hideRecyclerRecipes", Configuration.CATEGORY_GENERAL, false).getBoolean();
+        hideEnchantmentRecipes = Loader.isModLoaded("IC2") && config.get("hideEnchantmentRecipes", Configuration.CATEGORY_GENERAL, false).getBoolean();
+        recyclerCategoryClass = hideRecyclerRecipes ? (Class<? extends IRecipeCategory>) Class.forName("ic2.jeiIntegration.recipe.misc.ScrapboxRecipeCategory") : null;
+        enchantmentCategoryClass = hideEnchantmentRecipes ? (Class<? extends IRecipeCategory>) Class.forName("") : null;
+    }
 
     @Override
     public void register(@Nonnull IModRegistry iModRegistry)
@@ -30,11 +48,24 @@ public class JEIHider implements IModPlugin
     {
         if (itemStacksToHide.isEmpty()) {
             final IRecipeRegistry iRecipeRegistry = iJeiRuntime.getRecipeRegistry();
-            for (final ItemStack itemStack : iItemRegistry.getItemList())
-                if (iRecipeRegistry.getRecipeCategoriesWithInput(itemStack).isEmpty() && iRecipeRegistry.getRecipeCategoriesWithOutput(itemStack).isEmpty())
+            for (final ItemStack itemStack : iItemRegistry.getItemList()) {
+                final List<IRecipeCategory> recipeCategoriesOfInput = iRecipeRegistry.getRecipeCategoriesWithInput(itemStack);
+                final List<IRecipeCategory> recipeCategoriesOfOutput = iRecipeRegistry.getRecipeCategoriesWithOutput(itemStack);
+                if (recipeCategoriesOfInput.isEmpty() && recipeCategoriesOfOutput.isEmpty())
                     itemStacksToHide.add(itemStack);
+                if ((hideRecyclerRecipes || hideEnchantmentRecipes) && !recipeCategoriesOfInput.isEmpty() && recipeCategoriesOfOutput.isEmpty()) {
+                    if (recipeCategoriesOfInput.size() == 1) {
+                        if (hideRecyclerRecipes && recipeCategoriesOfInput.get(0).getClass() == recyclerCategoryClass)
+                            itemStacksToHide.add(itemStack);
+                        else if (hideEnchantmentRecipes && recipeCategoriesOfInput.get(0).getClass() == enchantmentCategoryClass)
+                            itemStacksToHide.add(itemStack);
+                    } else if (recipeCategoriesOfInput.size() == 2 && hideRecyclerRecipes && hideEnchantmentRecipes)
+                        if ((recipeCategoriesOfInput.get(0).getClass() == recyclerCategoryClass && recipeCategoriesOfInput.get(1).getClass() == enchantmentCategoryClass) ||
+                                (recipeCategoriesOfInput.get(1).getClass() == recyclerCategoryClass && recipeCategoriesOfInput.get(0).getClass() == enchantmentCategoryClass))
+                            itemStacksToHide.add(itemStack);
+                }
+            }
         }
-        for (final ItemStack itemStack : itemStacksToHide)
-            itemBlacklist.addItemToBlacklist(itemStack);
+        itemStacksToHide.forEach(itemBlacklist::addItemToBlacklist);
     }
 }
