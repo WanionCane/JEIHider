bb02e56edd21966b886fea4e256a72037a47f4dc
FTB_lag <dashotmine@ro.ru>
2019-11-11 04:37:11 +0300

    Port to 1.14.4

--- src/main/java/wanion/jeihider/JEIHider.java
+++ src/main/java/wanion/jeihider/JEIHider.java
@@ -1,59 +1,72 @@
-package wanion.jeihider;
+package ftblag.fluidcows.integration.jei;
 
 /*
  * Created by WanionCane(https://github.com/WanionCane).
  */
 
-import mezz.jei.api.*;
-import mezz.jei.api.ingredients.IIngredientRegistry;
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.recipe.IFocus;
-import mezz.jei.api.recipe.IRecipeCategory;
+import mezz.jei.api.recipe.IRecipeManager;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.runtime.IJeiRuntime;
 import mezz.jei.gui.Focus;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.fml.common.Loader;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLPaths;
 
 import javax.annotation.Nonnull;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-@JEIPlugin
+@JeiPlugin
 public class JEIHider implements IModPlugin
 {
-    private IItemBlacklist itemBlacklist;
-    private IIngredientRegistry iItemRegistry;
+    private static final ResourceLocation UID = new ResourceLocation("jeihider", "jeihider");
     private final List<ItemStack> itemStacksToHide = new ArrayList<>();
     private final boolean hideRecyclerRecipes;
     private final boolean hideEnchantmentRecipes;
     private final Class<? extends IRecipeCategory> recyclerCategoryClass;
     private final Class<? extends IRecipeCategory> enchantmentCategoryClass;
 
+    public static final String CATEGORY_GENERAL = "general";
+
     @SuppressWarnings("unchecked")
     public JEIHider() throws ClassNotFoundException
     {
-        final Configuration config = new Configuration(new File("." + File.separatorChar + "config" + File.separatorChar + "jeih.cfg"));
-        hideRecyclerRecipes = Loader.isModLoaded("IC2") && config.get(Configuration.CATEGORY_GENERAL, "hideRecyclerRecipes", false).getBoolean();
-        hideEnchantmentRecipes = Loader.isModLoaded("jeresources") && config.get(Configuration.CATEGORY_GENERAL, "hideEnchantmentRecipes", false).getBoolean();
+        ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
+        COMMON_BUILDER.comment("JEI Hider Configs").push(CATEGORY_GENERAL);
+
+        ForgeConfigSpec.BooleanValue boolHideRecyclerRecipes = COMMON_BUILDER.define("hideRecyclerRecipes", false);
+        ForgeConfigSpec.BooleanValue boolHideEnchantmentRecipes = COMMON_BUILDER.define("hideEnchantmentRecipes", false);
+
+        COMMON_BUILDER.pop();
+        CommentedFileConfig configData = CommentedFileConfig.builder(FMLPaths.CONFIGDIR.get().resolve("jeih.toml")).sync().autosave().writingMode(WritingMode.REPLACE).build();
+        configData.load();
+        COMMON_BUILDER.build().setConfig(configData);
+
+        hideRecyclerRecipes = ModList.get().isLoaded("ic2") && boolHideRecyclerRecipes.get();
+        hideEnchantmentRecipes = ModList.get().isLoaded("jeresources") && boolHideEnchantmentRecipes.get();
         recyclerCategoryClass = hideRecyclerRecipes ? (Class<? extends IRecipeCategory>) Class.forName("ic2.jeiIntegration.recipe.misc.ScrapboxRecipeCategory") : null;
         enchantmentCategoryClass = hideEnchantmentRecipes ? (Class<? extends IRecipeCategory>) Class.forName("jeresources.jei.enchantment.EnchantmentCategory") : null;
-        if (config.hasChanged())
-            config.save();
     }
 
     @Override
-    public void register(@Nonnull IModRegistry iModRegistry)
-    {
-        itemBlacklist = iModRegistry.getJeiHelpers().getItemBlacklist();
-        iItemRegistry = iModRegistry.getIngredientRegistry();
+    public ResourceLocation getPluginUid() {
+        return UID;
     }
 
     @Override
     public void onRuntimeAvailable(@Nonnull IJeiRuntime iJeiRuntime)
     {
         if (itemStacksToHide.isEmpty()) {
-            final IRecipeRegistry iRecipeRegistry = iJeiRuntime.getRecipeRegistry();
-            for (final ItemStack itemStack : iItemRegistry.getIngredients(ItemStack.class)) {
+            final IRecipeManager iRecipeRegistry = iJeiRuntime.getRecipeManager();
+            for (final ItemStack itemStack : iJeiRuntime.getIngredientManager().getAllIngredients(VanillaTypes.ITEM)) {
                 final List<IRecipeCategory> recipeCategoriesOfInput = iRecipeRegistry.getRecipeCategories(new Focus<>(IFocus.Mode.INPUT, itemStack));
                 final List<IRecipeCategory> recipeCategoriesOfOutput = iRecipeRegistry.getRecipeCategories(new Focus<>(IFocus.Mode.OUTPUT, itemStack));
                 if (recipeCategoriesOfInput.isEmpty() && recipeCategoriesOfOutput.isEmpty())
@@ -71,6 +84,6 @@ public class JEIHider implements IModPlugin
                 }
             }
         }
-        itemStacksToHide.forEach(itemBlacklist::addItemToBlacklist);
+        iJeiRuntime.getIngredientManager().removeIngredientsAtRuntime(VanillaTypes.ITEM, itemStacksToHide);
     }
 }
