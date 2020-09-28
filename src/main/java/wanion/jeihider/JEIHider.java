7e9a0e945bdd0a01a929428d97809b06083293a8
PolyacovYury <kuf-1@yandex.ru>
2020-09-28 03:28:08 +0300

    basically, overhaul - refactor   - stream from incoming ImmutableList into our own copy, while also filtering it from categories we want to ignore   - nothing changed logic-wise - add support and config for:   - vanilla anvil (yes, really, e.g. VariedCommodities) and EmbersRekindled anvil     - the item basically adds itself as both input and output   - ThaumicJei Aspects from item     - because of it, basically nothing is ever blacklisted

--- src/main/java/wanion/jeihider/JEIHider.java
+++ src/main/java/wanion/jeihider/JEIHider.java
@@ -17,58 +17,70 @@ import javax.annotation.Nonnull;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @JEIPlugin
-public class JEIHider implements IModPlugin
-{
+public class JEIHider implements IModPlugin {
     private IItemBlacklist itemBlacklist;
     private IIngredientRegistry iItemRegistry;
     private final List<ItemStack> itemStacksToHide = new ArrayList<>();
     private final boolean hideRecyclerRecipes;
     private final boolean hideEnchantmentRecipes;
+    private final boolean hideAspectRecipes;
+    private final boolean hideAnvilRecipes;
+    private final boolean hideEmbersAnvilRecipes;
     private final Class<? extends IRecipeCategory> recyclerCategoryClass;
     private final Class<? extends IRecipeCategory> enchantmentCategoryClass;
+    private final Class<? extends IRecipeCategory> aspectCategoryClass;
+    private final Class<? extends IRecipeCategory> anvilCategoryClass;
+    private final Class<? extends IRecipeCategory> embersAnvilCategoryClass;
 
     @SuppressWarnings("unchecked")
-    public JEIHider() throws ClassNotFoundException
-    {
+    public JEIHider() throws ClassNotFoundException {
         final Configuration config = new Configuration(new File("." + File.separatorChar + "config" + File.separatorChar + "jeih.cfg"));
         hideRecyclerRecipes = Loader.isModLoaded("IC2") && config.get(Configuration.CATEGORY_GENERAL, "hideRecyclerRecipes", false).getBoolean();
         hideEnchantmentRecipes = Loader.isModLoaded("jeresources") && config.get(Configuration.CATEGORY_GENERAL, "hideEnchantmentRecipes", false).getBoolean();
+        hideAspectRecipes = Loader.isModLoaded("thaumicjei") && config.get(Configuration.CATEGORY_GENERAL, "hideAspectRecipes", false).getBoolean();
+        hideAnvilRecipes = config.get(Configuration.CATEGORY_GENERAL, "hideAnvilRecipes", false).getBoolean();
+        hideEmbersAnvilRecipes = Loader.isModLoaded("embers") && config.get(Configuration.CATEGORY_GENERAL, "hideEmbersAnvilRecipes", false).getBoolean();
         recyclerCategoryClass = hideRecyclerRecipes ? (Class<? extends IRecipeCategory>) Class.forName("ic2.jeiIntegration.recipe.misc.ScrapboxRecipeCategory") : null;
         enchantmentCategoryClass = hideEnchantmentRecipes ? (Class<? extends IRecipeCategory>) Class.forName("jeresources.jei.enchantment.EnchantmentCategory") : null;
+        aspectCategoryClass = hideAspectRecipes ? (Class<? extends IRecipeCategory>) Class.forName("com.buuz135.thaumicjei.category.AspectFromItemStackCategory") : null;
+        anvilCategoryClass = hideAnvilRecipes ? (Class<? extends IRecipeCategory>) Class.forName("mezz.jei.plugins.vanilla.anvil.AnvilRecipeCategory") : null;
+        embersAnvilCategoryClass = hideEmbersAnvilRecipes ? (Class<? extends IRecipeCategory>) Class.forName("teamroots.embers.compat.jei.category.DawnstoneAnvilCategory") : null;
         if (config.hasChanged())
             config.save();
     }
 
     @Override
-    public void register(@Nonnull IModRegistry iModRegistry)
-    {
+    public void register(@Nonnull IModRegistry iModRegistry) {
         itemBlacklist = iModRegistry.getJeiHelpers().getItemBlacklist();
         iItemRegistry = iModRegistry.getIngredientRegistry();
     }
 
     @Override
-    public void onRuntimeAvailable(@Nonnull IJeiRuntime iJeiRuntime)
-    {
+    public void onRuntimeAvailable(@Nonnull IJeiRuntime iJeiRuntime) {
         if (itemStacksToHide.isEmpty()) {
             final IRecipeRegistry iRecipeRegistry = iJeiRuntime.getRecipeRegistry();
             for (final ItemStack itemStack : iItemRegistry.getIngredients(ItemStack.class)) {
-                final List<IRecipeCategory> recipeCategoriesOfInput = iRecipeRegistry.getRecipeCategories(new Focus<>(IFocus.Mode.INPUT, itemStack));
-                final List<IRecipeCategory> recipeCategoriesOfOutput = iRecipeRegistry.getRecipeCategories(new Focus<>(IFocus.Mode.OUTPUT, itemStack));
+                List<IRecipeCategory> recipeCategoriesOfInput = (
+                        iRecipeRegistry.getRecipeCategories(new Focus<>(IFocus.Mode.INPUT, itemStack))
+                ).stream().filter(cat -> !(
+                        hideRecyclerRecipes && cat.getClass() == recyclerCategoryClass
+                        || hideEnchantmentRecipes && cat.getClass() == enchantmentCategoryClass
+                        || hideAspectRecipes && cat.getClass() == aspectCategoryClass
+                        || hideAnvilRecipes && cat.getClass() == anvilCategoryClass
+                        || hideEmbersAnvilRecipes && cat.getClass() == embersAnvilCategoryClass)
+                ).collect(Collectors.toList());
+
+                List<IRecipeCategory> recipeCategoriesOfOutput = (
+                        iRecipeRegistry.getRecipeCategories(new Focus<>(IFocus.Mode.OUTPUT, itemStack))
+                ).stream().filter(cat -> !(
+                        hideAnvilRecipes && cat.getClass() == anvilCategoryClass
+                        || hideEmbersAnvilRecipes && cat.getClass() == embersAnvilCategoryClass)
+                ).collect(Collectors.toList());
                 if (recipeCategoriesOfInput.isEmpty() && recipeCategoriesOfOutput.isEmpty())
                     itemStacksToHide.add(itemStack);
-                if ((hideRecyclerRecipes || hideEnchantmentRecipes) && !recipeCategoriesOfInput.isEmpty() && recipeCategoriesOfOutput.isEmpty()) {
-                    if (recipeCategoriesOfInput.size() == 1) {
-                        if (hideRecyclerRecipes && recipeCategoriesOfInput.get(0).getClass() == recyclerCategoryClass)
-                            itemStacksToHide.add(itemStack);
-                        else if (hideEnchantmentRecipes && recipeCategoriesOfInput.get(0).getClass() == enchantmentCategoryClass)
-                            itemStacksToHide.add(itemStack);
-                    } else if (recipeCategoriesOfInput.size() == 2 && hideRecyclerRecipes && hideEnchantmentRecipes)
-                        if ((recipeCategoriesOfInput.get(0).getClass() == recyclerCategoryClass && recipeCategoriesOfInput.get(1).getClass() == enchantmentCategoryClass) ||
-                                (recipeCategoriesOfInput.get(1).getClass() == recyclerCategoryClass && recipeCategoriesOfInput.get(0).getClass() == enchantmentCategoryClass))
-                            itemStacksToHide.add(itemStack);
-                }
             }
         }
         itemStacksToHide.forEach(itemBlacklist::addItemToBlacklist);
